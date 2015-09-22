1f93d9f628b527f1cb06674096744d3bde37d6af
David Katz <15Dkatz@shcp.edu>
2015-09-22 09:50:39 -0700

    Completed.

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -9,18 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Variables
+    var currentTaps = 0
+    var maxTaps = 0
+    var multiple = 0
+    var total = 5
+    
+    //Properties
     @IBOutlet weak var playDisplay: UIButton!
     @IBOutlet weak var multiplesInput: UITextField!
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var multiplesTitle: UIImageView!
+    @IBOutlet weak var addDisplay: UIButton!
     
     
+
+    //Actions
     @IBAction func playButton(sender: UIButton) {
         
+        // var textDisplay = Int(textLabel.text!)!
+        
+        
+        if multiplesInput.text != nil && multiplesInput.text != "" {
+            multiple = Int(multiplesInput.text!)!
+            maxTaps = Int(multiplesInput.text!)!
+            
+            playDisplay.hidden = true
+            multiplesTitle.hidden = true
+            multiplesInput.hidden = true
+            
+            addDisplay.hidden = false
+            textLabel.hidden = false
+        
+            currentTaps = 0
+        }
         
     }
     
+    @IBAction func addButton(sender: UIButton) {
+        tapCount()
+        if currentTaps >= maxTaps {
+            gameRestart()
+        }
+        textDisplay()
+    }
 
+    func tapCount() {
+        currentTaps += 1
+        total += multiple
+    }
+    
+    func gameRestart() {
+        playDisplay.hidden = false
+        multiplesTitle.hidden = false
+        multiplesInput.hidden = false
+        
+        addDisplay.hidden = true
+        textLabel.hidden = true
+
+    }
+    
+    func textDisplay() {
+        textLabel.text = "\(currentTaps*multiple) + 5 = \(total)"
+    }
+    
 }
 
