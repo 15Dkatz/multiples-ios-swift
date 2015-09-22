e24042f2f8ec6cdf06e7c92670653fc60cba3085
David Katz <15Dkatz@shcp.edu>
2015-09-22 09:28:39 -0700

    Finished Layout

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var playDisplay: UIButton!
+    @IBOutlet weak var multiplesInput: UITextField!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var multiplesTitle: UIImageView!
+    
+    
+    @IBAction func playButton(sender: UIButton) {
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
