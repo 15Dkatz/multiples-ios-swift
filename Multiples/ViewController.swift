e5d8444c8157246e4708b788bf85da09cbb2cf94
David Katz <15Dkatz@shcp.edu>
2015-09-22 13:02:10 -0700

    Product Complete!

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -13,14 +13,16 @@ class ViewController: UIViewController {
     var currentTaps = 0
     var maxTaps = 0
     var multiple = 0
-    var total = 5
+    var total = 1
     
     //Properties
     @IBOutlet weak var playDisplay: UIButton!
     @IBOutlet weak var multiplesInput: UITextField!
+    @IBOutlet weak var multiplesInput2: UITextField!
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var multiplesTitle: UIImageView!
     @IBOutlet weak var addDisplay: UIButton!
+    @IBOutlet weak var textLabel2: UILabel!
     
     
 
@@ -32,14 +34,17 @@ class ViewController: UIViewController {
         
         if multiplesInput.text != nil && multiplesInput.text != "" {
             multiple = Int(multiplesInput.text!)!
-            maxTaps = Int(multiplesInput.text!)!
+            total = multiple
+            maxTaps = Int(multiplesInput2.text!)!
             
             playDisplay.hidden = true
             multiplesTitle.hidden = true
             multiplesInput.hidden = true
+            multiplesInput2.hidden = true
             
             addDisplay.hidden = false
             textLabel.hidden = false
+            textLabel2.hidden = false
         
             currentTaps = 0
         }
@@ -63,14 +68,17 @@ class ViewController: UIViewController {
         playDisplay.hidden = false
         multiplesTitle.hidden = false
         multiplesInput.hidden = false
+        multiplesInput2.hidden = false
         
         addDisplay.hidden = true
         textLabel.hidden = true
-
+        textLabel2.hidden = true
+        
     }
     
     func textDisplay() {
-        textLabel.text = "\(currentTaps*multiple) + 5 = \(total)"
+        textLabel.text = "\(currentTaps*multiple) + \(multiple) = \(total)"
+        textLabel2.text = "Multiple \(currentTaps+1):"
     }
     
 }
